d1b72542e77e3ca440301532640b59413d77662b
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-15 23:26:16 +0900

    feat(main): Add exception handler for 'URLError'

--- chapter1/first_scraper.py
+++ chapter1/first_scraper.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from urllib.request import HTTPError
+from urllib.request import URLError
 from bs4 import BeautifulSoup
 
 # exception handling
@@ -7,6 +8,8 @@ try:
     html = urlopen("http://www.pythonscraping.com/pages/page1.html")
 except HTTPError as e:
     print(e)
+except URLError as e:
+    print("The server could not be found!")
 else:
     bs = BeautifulSoup(html.read(), 'html.parser')
     print(bs)

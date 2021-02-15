b89e3a89e035c1aa9d8d29f700b9e3ab11d12e78
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-15 23:29:14 +0900

    refactor(main): Improve readability of code

--- chapter1/first_scraper.py
+++ chapter1/first_scraper.py
@@ -3,13 +3,21 @@ from urllib.request import HTTPError
 from urllib.request import URLError
 from bs4 import BeautifulSoup
 
-# exception handling
-try:
-    html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print("The server could not be found!")
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+
+if title == None:
+    print("Title could not be found")
 else:
-    bs = BeautifulSoup(html.read(), 'html.parser')
-    print(bs)
+    print(title)

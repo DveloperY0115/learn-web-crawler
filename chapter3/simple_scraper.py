4d92c676797357546f1370c78cf3500f8b341ce3
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-19 17:36:43 +0900

    feat(main): Implement function 'get_links'

--- chapter3/simple_scraper.py
+++ chapter3/simple_scraper.py
@@ -4,10 +4,12 @@ import datetime
 import random
 import re
 
-def getLinks(wiki_article_url):
+# set for holding already discovered links at run-time
+pages = set()
 
-    # create an empty list which will be used to store links
-    links = []
+def get_article_links(wiki_article_url):
+
+    global pages # all function calls share the same set
 
     # open the given URL and sent HTML request
     html = urlopen('https://en.wikipedia.org{}'.format(wiki_article_url))
@@ -17,3 +19,24 @@ def getLinks(wiki_article_url):
 
     return bs.find('div', {'id': 'bodyContent'}).findAll(
         'a', href = re.compile('^(/wiki/)((?!:).)*$'))
+
+
+def get_links(wiki_article_url):
+
+    global pages
+
+    # open the given URL and sent HTML request
+    html = urlopen('https://en.wikipedia.org{}'.format(wiki_article_url))
+
+    # parse it through BeatifulSoup
+    bs = BeautifulSoup(html, 'html.parser')
+
+    for link in bs.findAll('a', href=re.compile('^(/wiki/)')):
+        # why handle this exception?
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # discovered new page
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                get_links(newPage)

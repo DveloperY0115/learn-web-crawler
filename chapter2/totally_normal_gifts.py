c10507d515d218a4c404e77465fd4428d2f6cb68
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-19 16:15:28 +0900

    feat(main): Complete example 'totally_normal_gifts'

--- chapter2/totally_normal_gifts.py
+++ chapter2/totally_normal_gifts.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 html = urlopen('http://pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html, 'html.parser')
@@ -11,3 +12,8 @@ print('Items in the table:')
 
 for sibling in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
     print(sibling)
+
+images = bs.findAll('img', {'src': re.compile('\.\.\/img\/gifts\/img.*\.jpg')})
+
+for image in images:
+    print(image['src'])

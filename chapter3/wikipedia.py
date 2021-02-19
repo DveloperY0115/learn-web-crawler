9e2e308c1825f7a94e29da66acdd3cdaf24f7891
Yoo SeungWoo <dreamy1534@gmail.com>
2021-02-19 18:02:16 +0900

    feat(main): Define main routine searching entire Wikipedia

--- chapter3/wikipedia.py
+++ chapter3/wikipedia.py
@@ -4,10 +4,5 @@ from simple_scraper import *
 random.seed(datetime.datetime.now())
 
 if __name__ == '__main__':
-    links = get_article_links('/wiki/Kevin_Bacon')
-    recur_limit = 10
-    while len(links) > 0 and recur_limit >= 0:
-        newArticle = links[random.randint(0, len(links) - 1)].attrs['href']
-        print(newArticle)
-        recur_limit -= 1
-        links = get_article_links(newArticle)
+    # start crawling from the landing page of Wikipedia
+    get_links('')
